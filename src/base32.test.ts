fa199e78b8958066f85c23a0f5c745e47f5931dc
Cinnamon <cinnamon_bun_github@fastmail.com>
2021-03-28 16:22:50 -0700

    first post

---
+++ src/base32.test.ts
@@ -0,0 +1,70 @@
+import * as fc from 'fast-check';
+import {
+    b32chars,
+    decodeBase32ToBuffer,
+    encodeBufferToBase32
+} from 'earthstar';
+
+test('base32 encoding/decoding starting from a buffer', () => {
+    fc.assert(
+        fc.property(
+            // https://github.com/dubzzz/fast-check/blob/main/documentation/Arbitraries.md
+            fc.uint8Array({minLength: 0, maxLength: 20}),
+            (uintArray) => {
+                let buffer = Buffer.from(uintArray);
+                let str = encodeBufferToBase32(buffer);
+
+                expect(str.length).toBeGreaterThanOrEqual(1);
+                expect(str[0]).toStrictEqual('b');
+                expect(str.toLowerCase()).toStrictEqual(str);
+
+                let buffer2 = decodeBase32ToBuffer(str);
+                expect(buffer).toStrictEqual(buffer2);
+
+                let str2 = encodeBufferToBase32(buffer2);
+                expect(str).toStrictEqual(str2);
+
+                //console.log(str, buffer);
+            }
+        ), {
+            // https://github.com/dubzzz/fast-check/blob/main/documentation/Runners.md
+            verbose: true,
+            numRuns: 200,
+            examples: [
+                [new Uint8Array([0, 0, 0, 0, 0, 0, 0, 1])],
+            ],
+            // endOnFailure: true,
+        }
+    );
+});
+
+test('base32 encoding/decoding starting from a string', () => {
+    fc.assert(
+        fc.property(
+            // https://github.com/dubzzz/fast-check/blob/main/documentation/Arbitraries.md
+            fc.stringOf(fc.constantFrom(...b32chars), { minLength: 0, maxLength: 10} ),
+            (strNoB) => {
+                let str = 'b' + strNoB;
+                try {
+                    let buffer = decodeBase32ToBuffer(str);
+                    let str2 = encodeBufferToBase32(buffer);
+                    let buffer2 = decodeBase32ToBuffer(str2);
+                    //console.log(str, buffer);
+                    expect(str).toStrictEqual(str2);
+                    expect(buffer).toStrictEqual(buffer2);
+                } catch (err) {
+                    if (err instanceof SyntaxError) {
+                        // ok
+                        //console.log(str, 'could not be decoded, which is ok');
+                    } else {
+                        console.log(str, 'threw an unexpected error');
+                        throw err;
+                    }
+                }
+            }
+        ), {
+            verbose: true,
+            numRuns: 500,
+        }
+    );
+});
